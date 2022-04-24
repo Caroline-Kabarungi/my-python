9d1b9bb44da58cb8edc05787f8901a72ee7f64ca
Caroline-Kabarungi <josephkabarungicaroline@gmail.com>
2022-04-24 20:28:49 +0300

    changes

--- python.py
+++ python.py
@@ -10,24 +10,13 @@ if query1=='yes':
 			print("Shine on you got this!!")
 		elif query3=="angry" or query3=="frustrated" or query3=="agitated" or query3=="on edge" or query3=="aaahhh":
 			print("Take a deep breath everything is going to be okay")
-			query4= input("Would you like to speak to our counsellor? Type yes or no\n")
-			if query4=="yes":
-			 category=	input("Please state category: Family, work, personal")
-			if category=="Family":
-				print("Call 555")
-			elif category=="work":
-				print("Call 777")
-			elif category=="personal":
-				print("Call 888")
-			else:
-				print("Call 999")
-		elif query3=="sad" or query3=="lonely" or query3=="blue":
-			print("Hugs and cuddles. You are not alone")
-		else:
-			print("The power is within you.Smile!!")
+	
+
+		
 			
-else:
-	print("Maybe try again later")
+			
+
+
 
 
 
